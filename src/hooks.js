eff860bd3610276994a5f871ec0d54302f2a86b6
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-12-08 10:37:29 -0500

    useAxios hook

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const useFlip = () => {
 	const [ state, setState ] = useState(true);
@@ -7,4 +7,36 @@ const useFlip = () => {
 	};
 	return [ state, flipCard ];
 };
-export default useFlip;
+
+function useAxios(keyInLS, baseUrl) {
+	const [ responses, setResponses ] = useLocalStorage(keyInLS);
+
+	const addResponseData = async (formatter = (data) => data, restOfUrl = '') => {
+		const response = await axios.get(`${baseUrl}${restOfUrl}`);
+		setResponses((data) => [ ...data, formatter(response.data) ]);
+	};
+
+	const clearResponses = () => setResponses([]);
+
+	return [ responses, addResponseData, clearResponses ];
+}
+
+function useLocalStorage(key, initialValue = []) {
+	if (localStorage.getItem(key)) {
+		initialValue = JSON.parse(localStorage.getItem(key));
+	}
+	const [ value, setValue ] = useState(initialValue);
+
+	useEffect(
+		() => {
+			localStorage.setItem(key, JSON.stringify(value));
+		},
+		[ value, key ]
+	);
+
+	return [ value, setValue ];
+}
+
+export default useLocalStorage;
+
+export { useFlip, useAxios, useLocalStorage };
